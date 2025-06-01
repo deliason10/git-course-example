15a68f4b7f81af4dd610eaeeff0f8f8f4c7b3af9
derek <53630951+deliason10@users.noreply.github.com>
2025-06-01 11:45:14 -0700

    final commit for add_numbers() to master

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,10 +1,7 @@
 def add_numbers(a, b, c):
-<<<<<<< HEAD
-    return sum([a, b, c])
-=======
     '''Return the sum of a, b, and c.'''
-    return a + b + c
->>>>>>> master
+    return sum([a, b, c])
+
 
 def calc_difference(a, b):
     return a - b
